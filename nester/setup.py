8efc16ffa13d1c8f5744b7cc307609a7f9e13f1b
JinShengKai <30313778+JinShengKai@users.noreply.github.com>
2017-09-12 15:26:15 +0800

    This is test and training of Python for the study what i will learning

---
+++ nester/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name    = 'nester',
+    version = '1.1.0',
+    py_modules =['nester'],
+    author='Edward',
+    author_email='shengkaijin@hotmail.com',
+    url='43',
+    description='A simple printer of nested list',
+)
