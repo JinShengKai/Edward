f8ab950ed9d6360391142e14c2ba7e620508ffc9
JinShengKai <30313778+JinShengKai@users.noreply.github.com>
2017-09-25 16:33:54 +0800

    201709251633

--- nester/CoachTest.py
+++ nester/CoachTest.py
@@ -1,5 +1,5 @@
 import io
-
+#该函数是实现将字符串中的不规则数据改成统一格式数据
 def sanitize(time_string):
     if '-' in time_string:
         splitter = '-'
@@ -9,45 +9,73 @@ def sanitize(time_string):
         return (time_string)
     (mins,secs)=time_string.split(splitter)
     return (mins+"."+secs)
-with open('File/james.txt') as jaf:
-    data=jaf.readline()
-james=data.strip().split(',')
-with open('File/julie.txt') as juf:
-    data=juf.readline()
-julie=data.strip().split(',')
-with open('File/mikey.txt') as mif:
-    data=mif.readline()
-mikey=data.strip().split(',')
-with open('File/sarah.txt') as saf:
-    data=saf.readline()
-sarah=data.strip().split(',')
+#该函数是取文件的函数
+def get_coach_data(filename):
+    try:
+        with open(filename)  as f:
+            data=f.readline()
+        return data.strip().split(',')
+    except IOError as err:
+        print("IOError is:"+str(err))
+#sorted()[0:3]从0开始取三个 set()设置集合
+print(sorted(set([sanitize(t) for t in get_coach_data("File/james.txt")]))[0:3])
+print(sorted(set([sanitize(t) for t in get_coach_data("File/julie.txt")]))[0:3])
+print(sorted(set([sanitize(t) for t in get_coach_data("File/mikey.txt")]))[0:3])
+print(sorted(set([sanitize(t) for t in get_coach_data("File/sarah.txt")]))[0:3])
+# with open('File/james.txt') as jaf:
+#     data=jaf.readline()
+# james=data.strip().split(',')
+# with open('File/julie.txt') as juf:
+#     data=juf.readline()
+# julie=data.strip().split(',')
+# with open('File/mikey.txt') as mif:
+#     data=mif.readline()
+# mikey=data.strip().split(',')
+# with open('File/sarah.txt') as saf:
+#     data=saf.readline()
+# sarah=data.strip().split(',')
+#
+# clean_james=[sanitize(each_itme) for each_itme in james]
+# clean_julie=[sanitize(each_itme) for each_itme in julie]
+# clean_mikey=[sanitize(each_itme) for each_itme in mikey]
+# clean_sarah=[sanitize(each_itme) for each_itme in sarah]
 
-'''data=[1,2,6,2,41,2]
-data2=sorted(data)
-#data.sort()
-print(data)
-print(data2)'''
-#正常迭代
-'''
-clean_julie=[]
-clean_mikey=[]
-clean_sarah=[]
-for each_item in james:
-    clean_james.append(sanitize(each_item))
-for each_item in julie:
-    clean_julie.append(sanitize(each_item))
-for each_item in mikey:
-    clean_mikey.append(sanitize(each_item))
-for each_item in sarah:
-    clean_sarah.append(sanitize(each_item))
-'''
+# 打印去重复的集合
+
+#
+# data=[1,2,6,2,41,2]
+# data2=sorted(data)
+# #data.sort()
+# print(data)
+# print(data2)
+#
+# #正常迭代
+#
+# clean_julie=[]
+# clean_mikey=[]
+# clean_sarah=[]
+# for each_item in james:
+#     clean_james.append(sanitize(each_item))
+# for each_item in julie:
+#     clean_julie.append(sanitize(each_item))
+# for each_item in mikey:
+#     clean_mikey.append(sanitize(each_item))
+# for each_item in sarah:
+#     clean_sarah.append(sanitize(each_item))
+
 #列表推倒eg：meters = [ 1,2,3 ] ;feet =[m*3.281 for m in meters]
-clean_james=[sanitize(each_itme) for each_itme in james]
-clean_julie=[sanitize(each_itme) for each_itme in julie]
-clean_mikey=[sanitize(each_itme) for each_itme in mikey]
-clean_sarah=[sanitize(each_itme) for each_itme in sarah]
 
+'''
 print(sorted(clean_james))
 print(sorted(clean_julie))
 print(sorted(clean_mikey))
 print(sorted(clean_sarah))
+print(clean_james[0:3])
+#去重
+for item in clean_james:
+    if item  not in unique_james:
+        unique_james.append(item)
+'''
+
+
+
---
+++ nester/Python_Class.py
@@ -0,0 +1,46 @@
+import io
+import os
+import pickle
+
+        #the code to initialize a "Athlete" object
+# sarah=Athlete('Sarah Sweeney','2002-6-17',['2:58','2.58','1.56'])
+# james=Athlete('James Jones')
+# print(type(sarah))
+# print(type(james))
+def sanitized(time_list):
+    if '-' in time_list:
+        splitter = '-'
+    elif ':' in time_list:
+        splitter = ':'
+    else :
+        return  time_list
+    (mins,secs)=time_list.split(splitter)
+    return (mins+'.'+secs)
+
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data=f.readline()
+        templ = data.strip().split(",")
+        #不pop就是全部
+
+        #print(templ)
+        return (Athlete(templ.pop(0),templ.pop(0),templ))
+    except IOError as err:
+        print('File Eroor:'+str(err))
+        return (None)
+
+class Athlete:
+    def __init__(self,a_name,a_dob=None,a_times=[]):
+        #类中每个方法的第一个参数都是self，下面定义了三个属性并用形参赋值
+        self.name=a_name
+        self.dob=a_dob
+        self.time=a_times
+    def top3(self):
+        return (sorted(set([sanitized(t) for t in self.time]))[0:3])
+    def add_time(self,time):
+        self.time.append(time)
+    def add_times(self,times):
+        self.time.extend(times)
+james=get_coach_data("File/james2.txt")
+print(james.name+"'s fastest times are :"+str(james.top3()))
